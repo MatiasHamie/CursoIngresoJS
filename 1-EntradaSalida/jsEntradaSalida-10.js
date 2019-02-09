71fbb681c0fe4959e95d35bfe08a0a5bfc92a938
alumno <alumno>
2019-02-08 21:50:07 -0300

    Practica Calculos matematicos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var tomarImporte;
+	var calculoDescuento;
+
+	tomarImporte = importe.value;
+	tomarImporte = parseInt(tomarImporte);
+
+	calculoDescuento = tomarImporte * (1 - 25/100);
+
+	resultado.value = calculoDescuento;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var ingreseSueldo;
+	var mostrarResultado;
+	var aumento;
 
+	ingreseSueldo = sueldo.value;
+	ingreseSueldo = parseInt(ingreseSueldo);
+	aumento = ingreseSueldo * 0.10;
+
+	mostrarResultado = ingreseSueldo + aumento;
+
+	resultado.value = mostrarResultado;
+	
 	
 }
