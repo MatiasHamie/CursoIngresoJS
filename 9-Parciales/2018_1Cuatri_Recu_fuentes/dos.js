05ffce7250e1abde48e084f3a98e8c0ab196cb54
alumno <alumno>
2019-02-14 19:58:50 -0300

    Ejercicio 2 TP conceptual

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -7,8 +7,8 @@ function mostrar()
 	precio = prompt("Ingrese precio del producto");
 	precio = parseInt(precio);
 
-	precioDescuento = precio + (precio * 10/100);
-	precioFinal = precioDescuento + (precioDescuento * 21/100);
+	precioDescuento = precio - (precio * 10/100);
+	precioFinal = precioDescuento - (precioDescuento * 21/100);
 
 	alert("Tu compra es de $"+precio+" tenes un descuento del 10% queda en $"+precioDescuento+" mas el iva es $"+precioFinal);
 }
