497ca45e9e99d545b2cce612a34b6ef2f91fdf6f
alumno <alumno>
2019-02-14 19:33:58 -0300

    Ejercicio 1 TP conceptual

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+	var mascotaUno;
+	var mascotaDos;
+	var pesoMascotaUno;
+	var pesoMascotaDos;
+	var suma;
+
+	mascotaUno = "Pepe";
+	mascotaDos = "Barney";
+	pesoMascotaUno = 24;
+	pesoMascotaDos = 20;
+
+	pesoMascotaUno = parseInt(pesoMascotaUno);
+	pesoMascotaDos = parseInt(pesoMascotaDos);
+
+	suma = pesoMascotaUno+pesoMascotaDos;
+
+	alert("Tenes dos mascotas "+mascotaUno+" y "+mascotaDos+",que pesan "+pesoMascotaUno+" kilos y "+pesoMascotaDos+" kilos, la suma de los kilos es "+suma);
 
 }
