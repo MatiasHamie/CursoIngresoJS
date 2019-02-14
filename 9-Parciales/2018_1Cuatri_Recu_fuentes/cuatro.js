01e3378154a0b7782be65ba6c278cc153104efc0
alumno <alumno>
2019-02-14 20:31:53 -0300

    Ejercicio 4 TP conceptual

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -3,6 +3,7 @@ function mostrar()
 	var cantPersonas;
 	var precio;
 	var precioFinal;
+	var precioConDescuento;
 
 	cantAmigos = prompt("Ingrese cantidad de personas:");
 	precio = prompt("Ingrese precio de la comida:");	
@@ -10,7 +11,8 @@ function mostrar()
 	cantAmigos = parseInt(cantAmigos);
 	precio = parseInt(precio);
 
-	precioFinal = (precio + (precio * 10/100)) / cantAmigos;
+	precioFinal = (precio + precio * 10/100);
+	precioConDescuento =  precioFinal / cantAmigos;
 
-	alert("Cantidad de personas: "+cantAmigos+", el precio de la comida es: $"+precio+", con una propina del 10%, cada uno debe pagar: $"+precioFinal);
+	alert("Cantidad de personas: "+cantAmigos+", el precio de la comida es: $"+precio+", con una propina del 10% el precio final es $"+precioFinal+", cada uno debe pagar: $"+precioConDescuento);
 }
