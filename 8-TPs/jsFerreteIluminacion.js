0fb81524807d45ad189843266a660f18c26166d2
ar35630323 <mnhamie@gmail.com>
2019-02-21 16:54:59 -0300

    TP Iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -4,51 +4,106 @@ A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos bruto en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
 {
- 	var precioLamparas;
- 	var cantLamparas;
- 	var descuento;
- 	var marcaEmpresa;
- 	var mensaje;
+	var importeLampara;
+	var cantidadLamparas;
+	var marcaLamparas;
+	var importeFinal;
+	var descuento;
+	var impuestoBruto;
+	var mensaje;
 
- 	precioLamparas = 35;
- 	descuento = 50;
- 	cantLamparas = Cantidad.value;
- 	cantLamparas = parseInt(cantLamparas);
+	importeLampara = 35;
 
- 	marcaEmpresa = Marca.value;
+	cantidadLamparas = Cantidad.value;
+	marcaLamparas = Marca.value;
 
- 	if(cantLamparas>5)
- 	{
- 		precioLamparas = 6 * (precioLamparas - (precioLamparas * descuento/100));
- 		mensaje = "El precio de 6 lamparas es: $"+precioLamparas
- 	}
- 	else
- 	{
- 		if(cantLamparas==5)
- 		{
- 			switch(marcaEmpresa)
- 			{
- 				case "ArgentinaLuz":
- 					descuento = 40;
- 					precioLamparas = 5 * (precioLamparas - (precioLamparas * descuento/100));
- 					mensaje =  "El precio de 5 lamparas es: $"+precioLamparas
- 					break;
- 					
- 				case "FelipeLamparas":
- 					descuento = 30;
- 					precioLamparas = 5 * (precioLamparas - (precioLamparas * descuento/100));
- 					mensaje =  "El precio de 5 lamparas es: $"+precioLamparas
- 					break;
- 			}
- 		}
- 	}
+	cantidadLamparas = parseInt(cantidadLamparas);
+	importeLampara = importeLampara * cantidadLamparas;
 
- 	alert(mensaje);
+	if(cantidadLamparas>5)
+	{
+		descuento = 50;
+		importeFinal = importeLampara - (importeLampara*descuento/100);
+		mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+	}
+	else
+	{
+		if(cantidadLamparas==5)
+		{
+			if (marcaLamparas=="ArgentinaLuz")
+			{
+				descuento = 40;
+				importeFinal = importeLampara - (importeLampara*descuento/100);
+				mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+			}
+			else
+			{
+				descuento = 30;
+				importeFinal = importeLampara - (importeLampara*descuento/100);
+				mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+			}
+		}
+		else
+		{
+			if(cantidadLamparas==4)
+			{
+				if (marcaLamparas=="ArgentinaLuz"||marcaLamparas=="FelipeLamparas")
+				{
+					descuento = 25;
+					importeFinal = importeLampara - (importeLampara*descuento/100);
+					mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+				}
+				else
+				{
+					descuento = 20;
+					importeFinal = importeLampara - (importeLampara*descuento/100);
+					mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+				}
+			}
+			else
+			{
+				if (cantidadLamparas==3)
+				{
+					if(marcaLamparas=="ArgentinaLuz")
+					{
+						descuento = 15;
+						importeFinal = importeLampara - (importeLampara*descuento/100);
+						mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+					}
+					else
+					{
+						if(marcaLamparas=="FelipeLamparas")
+						{
+							descuento = 10;
+							importeFinal = importeLampara - (importeLampara*descuento/100);
+							mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+						}
+						else
+						{
+							descuento = 5;
+							importeFinal = importeLampara - (importeLampara*descuento/100);
+							mensaje ="Usted desea comprar "+cantidadLamparas+" lamparitas, el precio total es: $ "+importeFinal+" Se le aplicó un descuento del "+descuento+"%";
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	if(importeFinal>120)
+	{
+		impuestoBruto = importeFinal * 10/100;
+		importeFinal = importeFinal + impuestoBruto;
+		mensaje ="Precio total con impuesto bruto es $"+importeFinal+ ", usted pago $"+impuestoBruto+" de IIBB."
+	}
 
-}
+	alert(mensaje);
+
+
+}	
