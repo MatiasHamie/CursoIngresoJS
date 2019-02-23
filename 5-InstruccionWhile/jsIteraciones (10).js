154beb1f394b89226cb099ade99e98f9f8c0113f
ar35630323 <mnhamie@gmail.com>
2019-02-23 14:53:50 -0300

    Practica en casa

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,6 +1,6 @@
 /*
 Al presionar el bot&oacute;n pedir  n&uacute;meros  hasta que el usuario quiera, mostrar:</br>
-1-Suma de los negativos.
+1-Suma de los negativos. 
 2-Suma de los positivos.
 3-Cantidad de positivos.
 4-Cantidad de negativos.
@@ -14,77 +14,71 @@ Al presionar el bot&oacute;n pedir  n&uacute;meros  hasta que el usuario quiera,
 function mostrar()
 {
 
-	var contador;
-	var acumulador;
-	var numero;
 	var respuesta;
-	var positivo;
-	var negativo;
+	var numeroIngresado;
 	var acumuladorNegativos;
 	var acumuladorPositivos;
 	var contadorPositivos;
 	var contadorNegativos;
 	var contadorCeros;
 	var contadorPares;
-	var cantCeros;
 	var promedioPositivos;
 	var promedioNegativos;
-	var difPosNeg;
+	var diferenciaPosNeg;
 
-	acumuladorPositivos=0;
-	acumuladorNegativos=0;
-	contador = 0;
-	contadorPositivos = 0;
-	contadorNegativos = 0;
-	contadorCeros = 0;
-	contadorPares=0;
-	acumulador = 0;
 	respuesta = "si";
+	acumuladorNegativos=0;
+	acumuladorPositivos=0;
+	contadorPositivos=0;
+	contadorNegativos=0;
+	contadorCeros=0;
+	contadorPares=0;
 
 	while(respuesta=="si")
 	{
-		contador = contador + 1;
-		numero = prompt("Ingrese número:"+contador+" #");
-		numero = parseInt(numero);
+		numeroIngresado=prompt("Ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
 
-		if(numero<0)
+		if(numeroIngresado==0)
 		{
-			contadorNegativos = contadorNegativos+1;
-			acumuladorNegativos = acumuladorNegativos+numero;
+			contadorCeros=contadorCeros+1;
 		}
-		else
-		{
-			if(numero>0)
+		else if (numeroIngresado<0)
 			{
-				contadorPositivos = contadorPositivos+1;
-				acumuladorPositivos = acumuladorPositivos+numero;
+				contadorNegativos=contadorNegativos+1;
+				acumuladorNegativos=acumuladorNegativos+numeroIngresado;
 			}
 			else
 			{
-				contadorCeros=contadorCeros+1;
+				contadorPositivos=contadorPositivos+1;
+				acumuladorPositivos=acumuladorPositivos+numeroIngresado;
 			}
-		} 
 		
-		if(numero%2==0)
-			{
-				contadorPares=contadorPares+1;
-			}
-		respuesta = prompt("Si desea continuar ingresando numeros ingrese ->si");
+		if(numeroIngresado%2==0)
+		{
+			contadorPares=contadorPares+1;
+		}
+
+		respuesta=prompt("Ingrese 'si' para continuar");
 	}
 
-	promedioNegativos = acumuladorNegativos/contadorNegativos;
-	promedioPositivos = acumuladorPositivos/contadorPositivos;
-	difPosNeg = acumuladorPositivos-acumuladorNegativos;
+	promedioPositivos=acumuladorPositivos/contadorPositivos;
+	promedioNegativos=acumuladorNegativos/contadorNegativos;
+	diferenciaPosNeg=acumuladorPositivos+acumuladorNegativos;
 
-	document.write("1-Suma de los negativos. "+acumuladorNegativos+"<br>"+
-					"2-Suma de los positivos. "+acumuladorPositivos+"<br>"+
-					"3-Cantidad de positivos. "+contadorPositivos+"<br>"+
-					"4-Cantidad de negativos. "+contadorNegativos+"<br>"+
-					"5-Cantidad de ceros. "+contadorCeros+"<br>"+
-					"6-Cantidad de números pares. "+contadorPares+"<br>"+
-					"7-Promedio de positivos. "+promedioPositivos+"<br>"+
-					"8-Promedios de negativos. "+promedioNegativos+"<br>"+
-					"9-Diferencia entre positivos y negativos, (positvos-negativos). "+difPosNeg+"<br>");
+	document.write(
+					"1-Suma de los negativos => "+acumuladorNegativos+"<br>"+
+					"2-Suma de los positivos => "+acumuladorPositivos+"<br>"+
+					"3-Cantidad de positivos => "+contadorPositivos+"<br>"+
+					"4-Cantidad de negativos => "+contadorNegativos+"<br>"+
+					"5-Cantidad de ceros => "+contadorCeros+"<br>"+
+					"6-Cantidad de números pares => "+contadorPares+"<br>"+
+					"7-Promedio de positivos => "+promedioPositivos+"<br>"+
+					"8-Promedios de negativos => "+promedioNegativos+"<br>"+
+					"9-Diferencia entre positivos y negativos => "+diferenciaPosNeg+"<br>");
+
+
+
 				
 
 }//FIN DE LA FUNCIÓN
