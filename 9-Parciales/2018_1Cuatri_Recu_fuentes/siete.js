2e90a05372e8991131db7da43982a9439ad5a4ec
Matias Hamie <Tuteh@MacBook-Pro-de-Tuteh.local>
2019-02-23 18:07:04 -0300

    Practica Recuperatorio en casa

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -13,62 +13,94 @@
 
 function mostrar()
 {
-	var nota;
-	var sexo;
-	var minimo;
-	var notaAltaVarones;
+	var contadorNota;
 	var contadorAlumnos;
-	var contadorNotas;
+	var valorNota;
+	var sexo;
 	var acumuladorNotas;
-	var promedio;
+	var promedioNotas;
+	var notaMasBaja;
+	var sexoNotaMasBaja;
 	var contadorVarones;
-	var sexoNotaBaja;
 
-	contadorNotas=0;
+	contadorNota=0;
 	contadorAlumnos=0;
-	contadorVarones=0;
 	acumuladorNotas=0;
+	contadorVarones=0;
 
 	while(contadorAlumnos<5)
-	{
-		contadorAlumnos=contadorAlumnos+1;
-		
-		nota=prompt("Ingrese nota");
-		nota=parseInt(nota);
-		
-		sexo=prompt("Ingrese sexo del/la alumno/a -> f o -> m");
-
-		acumuladorNotas=acumuladorNotas+nota;
-
-		if(nota>5 && sexo=="m");
-		{
-			contadorVarones=contadorVarones+1;
-		}
-
-		if(contadorNotas==0)
-		{
-			minimo=nota;
-			contadorNotas=contadorNotas+1;
-		}
-		else
-		{
-			if(nota<minimo)
+		{	
+			contadorNota=contadorNota+1;
+			valorNota=prompt("Ingrese la "+contadorNota+"# nota, con un valor entre 0 y 10");
+			valorNota=parseInt(valorNota);
+			
+			//Verificacion nota válida:	
+			while(valorNota<0 || valorNota>10)
 			{
-				if(sexo="f")
-				{
-					minimo=nota;
-					sexoNotaBaja="f";
-				}
-				else
-				{
-					minimo=nota;
-					sexoNotaBaja="m";
-				}
+				valorNota=prompt("Error, ingrese un valor entre 0 y 10");
+				console.log(valorNota+"<- valorNota");
 			}
-		}
-	}
-	
-	promedio = acumuladorNotas/contadorAlumnos;
-	alert("El promedio de las notas totales: "+promedio+", La nota mas baja del sexo "+sexoNotaBaja+" fue de: "+minimo+", la cantidad de varones que su nota haya sido mayor o igual a 6. es de: "+contadorVarones);
+			
+			acumuladorNotas=acumuladorNotas+valorNota;
+			console.log(acumuladorNotas+"<- acumuladorNotas");
 
-}
+			sexo=prompt("Ingrese el sexo del alumno 'f' o 'm'");
+
+			//Verificacion nota válida:	
+			while(sexo!="f" && sexo!="m")
+			{
+				valorNota=prompt("Error, ingrese un sexo con las letras 'f' o 'm'");
+				console.log(sexo+"<- Sexo");
+			}
+			
+			//Verificacion si la nota es la primera en ser ingresada
+			if(contadorAlumnos==0)
+			{
+				notaMasBaja=valorNota;
+				sexoNotaMasBaja=sexo;
+			}
+			//Verificacion si la nota es menor a la ingresada anteriormente
+			else
+			{
+			 	if(valorNota<notaMasBaja)
+				{
+				notaMasBaja=valorNota;
+				sexoNotaMasBaja=sexo;
+				console.log(notaMasBaja+"<-notaMasBaja");
+				console.log(sexoNotaMasBaja+"<-sexoNotaMasBaja");
+				}			
+			}
+
+			if(sexo=="m" && valorNota>5)
+			{
+				contadorVarones=contadorVarones+1;
+			}
+
+			contadorAlumnos=contadorAlumnos+1;
+		}
+
+		promedioNotas=acumuladorNotas/contadorAlumnos;
+
+		alert("El promedio de las notas totales: "+promedioNotas+
+				"\nLa nota más baja: "+notaMasBaja+
+				"\nEl sexo de esa persona: "+sexoNotaMasBaja+
+				"\nLa cantidad de varones nota mayor o igual a '6': "+contadorVarones
+			 );
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
