80b3aa27fab996284931f16cccb023986b81c3a7
MatiasHamie <mnhamie@gmail.com>
2019-03-01 21:57:41 -0300

    Correcciones Parcial 7.js-8.js-9.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -11,32 +11,41 @@ function mostrar()
 	var combustibleTipoAlta;
 	var combustibleTipoLiqContador;
 
+	//Inicializo las variables
 	combustibleTipoLiqContador=0;
 	contadorVehiculo=0;
 	velocidadAcumulador=0;
 
 	while(contadorVehiculo<5)
 	{
+		//Ingreso velocidad
 		velocidadIngresada=prompt("Ingresar velocidad entre 0 y 250 KM/H");
 		velocidadIngresada=parseInt(velocidadIngresada);
 
+		//Valido velocidad
 		while(velocidadIngresada<0 || velocidadIngresada>250)
 		{
 			velocidadIngresada=prompt("Velocidad INVALIDA, Ingresar velocidad entre 0 y 250 KM/H");			
 		}
 
+		//Ingreso combustible
 		combustibleTipo=prompt("Ingresar (en minúscula) tipo de combustible, 's' para solido, 'l' para liquido");
 
+		//Valido combustible
 		while(combustibleTipo!="s" && combustibleTipo!="l")
 		{
 			combustibleTipo=prompt("ERROR, Ingresar (en minúscula) tipo de combustible, 's' para solido, 'l' para liquido");			
 		}
 
+		//si es la primera vez que se ingresan datos
+		//pongo el tipo de combustible en alta y baja (caro/barato)
 		if(contadorVehiculo==0)
 		{
 			combustibleTipoAlta=combustibleTipo;
 			combustibleTipoBaja=combustibleTipo;
 			velocidadBaja=velocidadIngresada;
+			//si el combustible es "s" (solido) la velocidad q se ingreso
+			//la guardo
 			if(combustibleTipo=="s")
 			{
 				velocidadAltaSolido=velocidadIngresada;	
@@ -44,6 +53,7 @@ function mostrar()
 		}
 		else
 		{
+			//velocidad minima
 			if(velocidadIngresada<velocidadBaja)
 			{
 				combustibleTipoBaja=combustibleTipo;
@@ -51,21 +61,26 @@ function mostrar()
 			}
 		}
 
+		//cantidad de (combustible liquido y velocidad menor a 100)
 		if(combustibleTipo=="l" && velocidadIngresada>100)
 		{
 			combustibleTipoLiqContador=combustibleTipoLiqContador+1;
 		}
 
+		//cantidad de [combustible solido y velocidad menor a velocidad alta (maxima)]
 		if(combustibleTipo=="s" && velocidadIngresada>velocidadAltaSolido)
 		{
 			velocidadAltaSolido=velocidadIngresada;
 		}
 
+		//acumulo las velocidades para hacer el promedio al final
 		velocidadAcumulador=velocidadAcumulador+velocidadIngresada;
 
+		//finalizo el while incrementando el contador de vehiculos
 		contadorVehiculo++;
 	}
 
+	//calculo promedios
 	velocidadPromedio=velocidadAcumulador/contadorVehiculo;
 
 	alert("\n1- El promedio de las velocidades totales: "+velocidadPromedio+
